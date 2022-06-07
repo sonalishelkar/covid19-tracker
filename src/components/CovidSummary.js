02fabbf73e55ad7acfb41bcdfd7e70028915602d
sonalishelkar <98012257+sonalishelkar@users.noreply.github.com>
2022-06-07 12:35:47 +0530

    covid19 tracker

---
+++ src/components/CovidSummary.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Card from './Card';
+import NumberFormat from 'react-number-format';
+
+const CovidSummary = (props) => {
+
+    const {
+        totalConfirmed,
+        totalRecovered,
+        totalDeaths,
+        country
+    } = props;
+
+
+    return (
+
+        <div>
+            <div>
+
+                <u><h1 style={{textTransform: 'capitalize'}}>{country === '' ? 'World Wide Corona Report' : country}</h1></u>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}>
+                   <div style={{margin:'20px'}}> <Card>
+                        <span>Total Confirmed</span><br />
+                        <span>
+                            {
+                                <NumberFormat 
+                                     value={totalConfirmed}
+                                     displayType={'text'}
+                                     thousandSeparator={true}
+                                />
+                            }
+                        </span>
+                    </Card></div>
+                    <div style={{margin:'20px'}}> <Card>
+                        <span>Total Recovered</span><br />
+                        <span>
+                            {
+                                <NumberFormat 
+                                     value={totalRecovered}
+                                     displayType={'text'}
+                                     thousandSeparator={true}
+                                />
+                            }
+                        </span>
+                    </Card> </div>
+                    <div style={{margin:'20px'}}><Card>
+                        <span>Total Deaths</span><br />
+                        <span>
+                           {
+                                <NumberFormat 
+                                     value={totalDeaths}
+                                     displayType={'text'}
+                                     thousandSeparator={true}
+                                />
+                            }  
+                        </span>
+                    </Card> </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    );
+}
+export default CovidSummary;
